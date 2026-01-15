b4d0be0fb1dd20b8c64e5aa04c0ab8027fc58000
rishikahakeem <rishikahakeem2004@gmail.com>
2026-01-15 20:41:44 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+name = "Hakeem Rishika"
+role = "Python Developer Intern"
+date = "15 January 2026"
+print("Name:", name)
+print("Role:", role)
+print("Date:", date)
+user_name = input("Enter your name: ")
+user_role = input("Enter your role: ")
+print("Your Name:", user_name)
+print("Your Role:", user_role)
